4a7b8624bfe5e71a40fa979ec4f736a26f325f75
aza <azamuzapbar@gmail.com>
2023-04-22 03:51:08 -0400

    #1 добавлена реализация регистрации входа выхода и профиль

---
+++ cw/accounts/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+
+class LoginForm(forms.Form):
+    email = forms.CharField(required=True, label='Логин')
+    password = forms.CharField(required=True, label='Пароль', widget=forms.PasswordInput)
+
+class CustomUserCreationForm(forms.ModelForm):
+    username = forms.CharField(min_length=5, max_length=50,
+                               widget=forms.TextInput, required=True)
+    email = forms.CharField(min_length=7, max_length=70, required=True,
+                            widget=forms.EmailInput)
+    password = forms.CharField(label='Пароль', strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Подтвердите пароль', strip=False, required=True,
+                                       widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'username', 'email',  'password', 'password_confirm')
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise ValidationError('Пароли не совпадают')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+        if commit:
+            user.save()
+        return user
--- cw/accounts/views.py
+++ cw/accounts/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, CreateView
 
-# Create your views here.
+from accounts.forms import LoginForm
+
+from accounts.forms import CustomUserCreationForm
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            messages.error(request, "Некорректные данные")
+            return redirect('index')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            messages.warning(request, "Пользователь не найден")
+            return redirect('index')
+        login(request, user)
+        messages.success(request, 'Добро пожаловать')
+        next = request.GET.get('next')
+        if next:
+            return redirect(next)
+        return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+    success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(self.success_url)
+        context = {'form':form}
+        return self.render_to_response(context)
